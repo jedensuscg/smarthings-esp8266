d2513231c53aa6d796e8a8ca71a5b5e4a5990dab
James Edens <jedensuscg@gmail.com>
2022-01-30 09:21:48 -0500

    inital commit

---
+++ src/DHTtester-james.cpp
@@ -0,0 +1,167 @@
+// Example testing sketch for various DHT humidity/temperature sensors
+// Written by ladyada, public domain
+
+// REQUIRES the following Arduino libraries:
+// - DHT Sensor Library: https://github.com/adafruit/DHT-sensor-library
+// - Adafruit Unified Sensor Lib: https://github.com/adafruit/Adafruit_Sensor
+#include <Arduino.h>
+#include <DHT.h>
+#include <ESP8266WiFi.h>
+#include <WiFiClient.h>
+#include <ESP8266WebServer.h>
+#include <ESP8266mDNS.h>
+#include <ArduinoJson.h>
+
+#define DHTPIN 4     // Digital pin connected to the DHT sensor
+#define DHTTYPE DHT22   // DHT 22  (AM2302), AM2321
+
+void handle_root();
+void gettemperature();
+// Connect pin 1 (on the left) of the sensor to +5V
+// NOTE: If using a board with 3.3V logic like an Arduino Due connect pin 1
+// to 3.3V instead of 5V!
+// Connect pin 2 of the sensor to whatever your DHTPIN is
+// Connect pin 3 (on the right) of the sensor to GROUND (if your sensor has 3 pins)
+// Connect pin 4 (on the right) of the sensor to GROUND and leave the pin 3 EMPTY (if your sensor has 4 pins)
+// Connect a 10K resistor from pin 2 (data) to pin 1 (power) of the sensor
+
+// Initialize DHT sensor.
+// Note that older versions of this library took an optional third parameter to
+// tweak the timings for faster processors.  This parameter is no longer needed
+// as the current DHT reading algorithm adjusts itself to work on faster procs.
+DHT dht(DHTPIN, DHTTYPE);
+const char* ssid     = "Kirbylicous";
+const char* password = "pinkbanana379";
+const int capacity = JSON_OBJECT_SIZE(96);
+String jsonData = "";
+StaticJsonDocument<capacity> data;
+ESP8266WebServer server(80);
+String webString=""; 
+float f,hif,h,t,hic;
+
+void handle_root() {
+  server.send(200, "text/plain", "Hello from the weather esp8266, read from /temp or /humidity");
+  delay(100);
+}
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println(F("DHT22 test!"));
+
+  dht.begin();
+
+    WiFi.begin(ssid, password);
+  Serial.print("\n\r \n\rWorking to connect");
+ 
+  // Wait for connection
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("DHT Weather Reading Server");
+  Serial.print("Connected to ");
+  Serial.println(ssid);
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  server.on("/", handle_root);
+  
+  server.on("/temp", [](){  // if you add this subdirectory to your webserver call, you get text below :)
+    gettemperature();       // read sensor
+    webString="{\"temp\":"+String((int)f)+"" ", \"humidity\": "+String((int)h)+",  \"heat_index\": "+String((int)hif)+"}""";   // Arduino has a hard time with float to string
+    DEBUG_PRINT("{\"temp\":"+String((int)f)+"" ", \"humidity\": "+String((int)h)+",  \"heat_index\": "+String((int)hif)+"}""")
+    server.send(200, "text/plain", webString);            // send to someones browser when asked
+  });
+
+
+ 
+  server.on("/humidity", [](){  // if you add this subdirectory to your webserver call, you get text below :)
+    gettemperature();           // read sensor
+    webString="Humidity: "+String((int)h)+"%";
+    server.send(200, "text/plain", webString);               // send to someones browser when asked
+  });
+
+  server.begin();
+  Serial.println("HTTP server started");
+}
+
+
+void gettemperature() {
+    // Wait a few seconds between measurements.
+    delay(2000);
+
+    // Reading temperature or humidity takes about 250 milliseconds!
+    // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
+    h = dht.readHumidity();
+    data["humidity"] = int(h);
+    // Read temperature as Celsius (the default)
+    t = dht.readTemperature();
+    // Read temperature as Fahrenheit (isFahrenheit = true)
+    f = dht.readTemperature(true);
+    data["temp"] = int(f);
+    // Check if any reads failed and exit early (to try again).
+    if (isnan(h) || isnan(t) || isnan(f)) {
+    Serial.println(F("Failed to read from DHT sensor!"));
+    return;
+    }
+
+    // Compute heat index in Fahrenheit (the default)
+    hif = dht.computeHeatIndex(f, h);
+    data["heat_index"] = int(hif);
+    // Compute heat index in Celsius (isFahreheit = false)
+    hic = dht.computeHeatIndex(t, h, false);
+    serializeJson(data, jsonData);
+    Serial.println(jsonData);
+    Serial.print(F("Humidity: "));
+    Serial.print(h);
+    Serial.print(F("%  Temperature: "));
+    Serial.print(t);
+    Serial.print(F("°C "));
+    Serial.print(f);
+    Serial.print(F("°F  Heat index: "));
+    Serial.print(hic);
+    Serial.print(F("°C "));
+    Serial.print(hif);
+    Serial.println(F("°F"));
+}
+
+
+
+
+void loop() {
+  server.handleClient();
+//  // Wait a few seconds between measurements.
+//  delay(2000);
+//
+//  // Reading temperature or humidity takes about 250 milliseconds!
+//  // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
+//  float h = dht.readHumidity();
+//  // Read temperature as Celsius (the default)
+//  float t = dht.readTemperature();
+//  // Read temperature as Fahrenheit (isFahrenheit = true)
+//  float f = dht.readTemperature(true);
+//
+//  // Check if any reads failed and exit early (to try again).
+//  if (isnan(h) || isnan(t) || isnan(f)) {
+//    Serial.println(F("Failed to read from DHT sensor!"));
+//    return;
+//  }
+//
+//  // Compute heat index in Fahrenheit (the default)
+//  float hif = dht.computeHeatIndex(f, h);
+//  // Compute heat index in Celsius (isFahreheit = false)
+//  float hic = dht.computeHeatIndex(t, h, false);
+//
+//  Serial.print(F("Humidity: "));
+//  Serial.print(h);
+//  Serial.print(F("%  Temperature: "));
+//  Serial.print(t);
+//  Serial.print(F("°C "));
+//  Serial.print(f);
+//  Serial.print(F("°F  Heat index: "));
+//  Serial.print(hic);
+//  Serial.print(F("°C "));
+//  Serial.print(hif);
+//  Serial.println(F("°F"));
+}
